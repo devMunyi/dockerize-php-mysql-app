33d9ac00d09a7394d6cb54778477b6a4071c6998
devMunyi <samunyi9@gmail.com>
2023-10-06 16:58:23 +0300

    Minor Update

--- php/src/index.php
+++ php/src/index.php
@@ -3,19 +3,25 @@
 include_once './conn.inc';
 
 $sql = 'SELECT * FROM users';
-
 $result = $conn->query($sql);
 
-if ($result = $conn->query($sql)) {
-    while ($data = $result->fetch_object()) {
-        $users[] = $data;
+if($result) {
+    if ($result = $conn->query($sql)) {
+        while ($data = $result->fetch_object()) {
+            $users[] = $data;
+        }
     }
+    
+    if(count($users) > 0){
+        foreach ($users as $user) {
+            echo "<br>";
+            echo $user->username . " " . $user->password;
+            echo "<br>";
+        }
+    }
+}else {
+    echo "0 results";
 }
 
-foreach ($users as $user) {
-    echo "<br>";
-    echo $user->username . " " . $user->password;
-    echo "<br>";
-}
 
 ?>
